0f26b7908567ee4c0b1ad29e1de0178a697ef82b
Simphiwe Kahlana <220162891@mycput.ac.za>
2021-03-10 22:42:38 +0200

    My First Commit

---
+++ src/za/ac/cput/gitpractise/Practise.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.gitpractise;
+
+/**
+ *
+ * @author simph
+ */
+public class Practise {
+    public static void main(String[] args) {
+        
+        System.out.println("This is a practise application");
+    }
+    
+}
